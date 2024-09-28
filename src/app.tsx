8b21ec309262ef1ca6e3bbde6201a406bcaf28c9
Jeysef <josimich7@gmail.com>
2024-09-28 21:49:31 +0200

    solid-start with vitest

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
@@ -8,10 +9,11 @@ export default function App() {
   return (
     <Router
       root={props => (
-        <>
+        <MetaProvider>
+          <Title>SolidStart - with Vitest</Title>
           <Nav />
           <Suspense>{props.children}</Suspense>
-        </>
+        </MetaProvider>
       )}
     >
       <FileRoutes />
