c8048466870d0a6434673e898f5818c5d39a7c37
Jeysef <josimich7@gmail.com>
2024-09-29 00:36:35 +0200

    add solid forms

---
+++ src/solid-forms/index.d.ts
@@ -0,0 +1,11 @@
+export * from './dist/src/abstract-control';
+export { DEFAULT_SOURCE, composeValidators, createAbstractControlBase } from './dist/src/abstract-control-base';
+export type { IAbstractControlBase } from './dist/src/abstract-control-base';
+export * from './dist/src/abstract-control-container';
+export { createAbstractControlContainerBase } from './dist/src/abstract-control-container-base';
+export type { IAbstractControlContainerBase } from './dist/src/abstract-control-container-base';
+export * from './dist/src/form-array';
+export * from './dist/src/form-control';
+export * from './dist/src/form-group';
+export { bindOwner } from './dist/src/util';
+//# sourceMappingURL=index.d.ts.map
