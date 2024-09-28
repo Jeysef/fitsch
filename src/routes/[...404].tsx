9e80b8e1d47e00773e7ea41d64abcdedb1df18d9
Jeysef <josimich7@gmail.com>
2024-09-28 21:54:35 +0200

    add some cool types and meta

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,8 +1,19 @@
+import { Title } from "@solidjs/meta";
 import { A } from "@solidjs/router";
+import { HttpStatusCode } from "@solidjs/start";
+import type { APIEvent } from "@solidjs/start/server";
+
+export const GET = (event: APIEvent) => {
+  if (event.request.headers.get("accept") !== "application/json") return;
+  return { notFound: "API" }
+};
+
 
 export default function NotFound() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
+      <Title>Not Found</Title>
+      <HttpStatusCode code={404} />
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Not Found</h1>
       <p class="mt-8">
         Visit{" "}
