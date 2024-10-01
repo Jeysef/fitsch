9f6f0454ab7d7bf71b39573b61b824e8f3952bb9
Jeysef <josimich7@gmail.com>
2024-10-01 12:38:54 +0200

    include solid-forms

--- src/solid-forms/index.d.ts
+++ src/solid-forms/index.d.ts
@@ -1,11 +1,11 @@
-export * from './dist/src/abstract-control';
-export { DEFAULT_SOURCE, composeValidators, createAbstractControlBase } from './dist/src/abstract-control-base';
-export type { IAbstractControlBase } from './dist/src/abstract-control-base';
-export * from './dist/src/abstract-control-container';
-export { createAbstractControlContainerBase } from './dist/src/abstract-control-container-base';
-export type { IAbstractControlContainerBase } from './dist/src/abstract-control-container-base';
-export * from './dist/src/form-array';
-export * from './dist/src/form-control';
-export * from './dist/src/form-group';
-export { bindOwner } from './dist/src/util';
+export * from './src/abstract-control';
+export { DEFAULT_SOURCE, composeValidators, createAbstractControlBase } from './src/abstract-control-base';
+export type { IAbstractControlBase } from './src/abstract-control-base';
+export * from './src/abstract-control-container';
+export { createAbstractControlContainerBase } from './src/abstract-control-container-base';
+export type { IAbstractControlContainerBase } from './src/abstract-control-container-base';
+export * from './src/form-array';
+export * from './src/form-control';
+export * from './src/form-group';
+export { bindOwner } from './src/util';
 //# sourceMappingURL=index.d.ts.map
