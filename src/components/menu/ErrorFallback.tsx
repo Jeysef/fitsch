b3d9823a310b5a9709f8e190983dc68d11369792
Jeysef <josimich7@gmail.com>
2024-11-18 10:18:00 +0100

    Revert "test throw error"
    This reverts commit 65e12392a3803275ffc131812d26a7f63433bd2e.

--- src/components/menu/ErrorFallback.tsx
+++ src/components/menu/ErrorFallback.tsx
@@ -41,7 +41,6 @@ const getErrorMessage = (error: any): string => {
 };
 
 const ErrorFallback: Component<ErrorBoundaryFallbackProps> = (props) => {
-  throw props.error
   const errorMessage = getErrorMessage(props.error);
   const isNetworkError = props.error.code === 'ENOTFOUND' ||
     props.error.code === 'ECONNREFUSED' ||
