08fb6b15ac2bd42452ff40fa8e47cd472aafae96
Jeysef <josimich7@gmail.com>
2024-11-20 11:31:08 +0100

    add metadata

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@
 // import "@fontsource/geist-mono";
 import "@fontsource/courier-prime";
 import "@fontsource/geist-sans";
-import { MetaProvider, Title } from "@solidjs/meta";
+import { Meta, MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
@@ -16,7 +16,10 @@ export default function App() {
     <Router
       root={(props) => (
         <MetaProvider>
-          <Title>SolidStart</Title>
+          <Title>FIT scheduler</Title>
+          <Meta name="author" content="Josef Michalík" />
+          <Meta name="description" content="FIT scheduler" />
+          <Meta name="keywords" content="FIT, VUT, scheduler, fitsch" />
           <Suspense>
             <I18nProvider>
               <Layout>
