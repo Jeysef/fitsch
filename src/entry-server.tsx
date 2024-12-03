ee00acf1ec75edd0d9348686a83ea632ca888a3f
Jeysef <josimich7@gmail.com>
2024-12-03 21:17:11 +0100

    move meta to entry-server

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -7,8 +7,22 @@ export default createHandler(() => (
       <html lang="en">
         <head>
           <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+          <title>FIT scheduler</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+          <meta name="author" content="Josef Michalík" />
+          <meta name="description" content="FIT scheduler" />
+          <meta name="keywords" content="FIT, VUT, scheduler, fitsch" />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <meta name="apple-mobile-web-app-title" content="FitScheduler" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#ffffff" />
           {assets}
         </head>
         <body>
