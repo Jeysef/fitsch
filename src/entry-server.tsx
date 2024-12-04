e56e539453331f3b5abbba7df86e469b45a1accb
Jeysef <josimich7@gmail.com>
2024-12-04 22:09:51 +0100

    icons

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -13,15 +13,13 @@ export default createHandler(() => (
           <meta name="author" content="Josef Michalík" />
           <meta name="description" content="FIT scheduler" />
           <meta name="keywords" content="FIT, VUT, scheduler, fitsch" />
-          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
           <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-          <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <meta name="apple-mobile-web-app-title" content="FitScheduler" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="msapplication-TileImage" content="/ms-icon-620x620.png" />
           <meta name="theme-color" content="#ffffff" />
           {assets}
         </head>
