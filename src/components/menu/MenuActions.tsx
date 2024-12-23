7521ed16f7b8deadf5cd31722dd0a00ea936a4b9
Jeysef <josimich7@gmail.com>
2024-12-23 20:36:32 +0100

    test bug fix, remove generator and tooltip

--- src/components/menu/MenuActions.tsx
+++ src/components/menu/MenuActions.tsx
@@ -1,6 +1,5 @@
 import ChevronDown from "lucide-solid/icons/chevron-down";
 import { ItemText, SectionHeading } from "~/components/menu/MenuCommonComponents";
-import SchedulerGenerator from "~/components/scheduler/generator";
 import { Button } from "~/components/ui/button";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "~/components/ui/collapsible";
 import { useI18n } from "~/i18n";
@@ -40,39 +39,39 @@ export function Actions() {
   //   else closeTooltip();
   // };
 
-  // const exportJSON = () => {
-  //   const a = document.createElement("a");
-  //   const file = new Blob([JSON.stringify(store)], { type: "application/json" });
-  //   a.href = URL.createObjectURL(file);
-  //   a.download = "schedule.json";
-  //   a.click();
-  //   a.remove();
-  // };
+  const exportJSON = () => {
+    const a = document.createElement("a");
+    const file = new Blob([JSON.stringify(store)], { type: "application/json" });
+    a.href = URL.createObjectURL(file);
+    a.download = "schedule.json";
+    a.click();
+    a.remove();
+  };
 
-  // const importJSON = async () => {
-  //   const input = document.createElement("input");
-  //   input.type = "file";
-  //   input.multiple = false;
-  //   input.accept = ".json";
-  //   input.click();
-  //   input.onchange = async () => {
-  //     const file = input.files?.[0];
-  //     if (!file) return;
-  //     const reader = new FileReader();
-  //     reader.onload = () => {
-  //       try {
-  //         const data = JSON.parse(reader.result as string);
-  //         recreateStore(data);
-  //       } catch (e) {
-  //         console.error(e);
-  //       }
-  //     };
-  //     reader.readAsText(file);
-  //   };
-  //   input.remove();
-  // };
+  const importJSON = async () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.multiple = false;
+    input.accept = ".json";
+    input.click();
+    input.onchange = async () => {
+      const file = input.files?.[0];
+      if (!file) return;
+      const reader = new FileReader();
+      reader.onload = () => {
+        try {
+          const data = JSON.parse(reader.result as string);
+          recreateStore(data);
+        } catch (e) {
+          console.error(e);
+        }
+      };
+      reader.readAsText(file);
+    };
+    input.remove();
+  };
 
-  const generator = SchedulerGenerator();
+  // const generator = SchedulerGenerator();
 
   return (
     <Collapsible defaultOpen>
@@ -81,7 +80,7 @@ export function Actions() {
         <ChevronDown />
       </CollapsibleTrigger>
       <CollapsibleContent>
-        {/* <ItemText as="div">
+        <ItemText as="div">
           <Button
             type="button"
             size={null}
@@ -102,8 +101,8 @@ export function Actions() {
           >
             {t("menu.actions.importJson")}
           </Button>
-        </ItemText> */}
-        <ItemText as="div" class="flex items-center gap-1">
+        </ItemText>
+        {/* <ItemText as="div" class="flex items-center gap-1">
           <Button
             type="button"
             size={null}
@@ -114,12 +113,12 @@ export function Actions() {
           >
             {generator.isGenerating() ? t("menu.actions.generate.generating") : t("menu.actions.generate.next")}
           </Button>
-          {/* <Tooltip placement="right" flip="top" gutter={12} open={tooltipOpen()} hideWhenDetached>
+          <Tooltip placement="right" flip="top" gutter={12} open={tooltipOpen()} hideWhenDetached>
             <TooltipTrigger type="button" on:click={() => handleTooltip((p) => !p)}>
               <CircleAlert class="w-4 h-4 text-amber-400" />
             </TooltipTrigger>
             <TooltipContent>{t("menu.actions.generate.warning")}</TooltipContent>
-          </Tooltip> */}
+          </Tooltip>
         </ItemText>
         <ItemText as="div">
           <Button
@@ -132,7 +131,7 @@ export function Actions() {
           >
             {generator.isGenerating() ? t("menu.actions.generate.generating") : t("menu.actions.generate.previous")}
           </Button>
-        </ItemText>
+        </ItemText> */}
       </CollapsibleContent>
     </Collapsible>
   );
