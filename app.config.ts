08cd3ba9ddec45efcd4034e41b5d6179ce0a10f7
Jeysef <josimich7@gmail.com>
2025-02-11 14:12:47 +0100

    Revert "feat: add caching functionality with defineCachedFunction and update app configuration"
    This reverts commit 3280cf7300e4932fdceb00a03c9f992b1b1b35f3.

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,3 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({
-  server: {
-    routeRules: {
-      "/test/**": { cache: { maxAge: 10, staleMaxAge: 20, swr: true, name: "test-api-cache" } },
-    },
-  },
-});
+export default defineConfig({});
--- src/server/utils/cache.d.ts
+++
@@ -1,9 +0,0 @@
-import type { CacheOptions } from "nitropack/types";
-export declare function defineCachedFunction<T, ArgsT extends unknown[] = any[]>(
-  fn: (...args: ArgsT) => T | Promise<T>,
-  opts?: CacheOptions<T, ArgsT>
-): (...args: ArgsT) => Promise<T>;
-export declare function cachedFunction<T, ArgsT extends unknown[] = any[]>(
-  fn: (...args: ArgsT) => T | Promise<T>,
-  opts?: CacheOptions<T>
-): (...args: ArgsT) => Promise<T | undefined>;
