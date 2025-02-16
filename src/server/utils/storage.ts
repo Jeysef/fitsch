a2cdd7d446c4913f28ba66b85c1c980e99f7acda
Jeysef <josimich7@gmail.com>
2025-02-16 23:44:06 +0100

    feat: implement mock memory storage driver and handle missing cache plugin

--- src/server/utils/storage.ts
+++ src/server/utils/storage.ts
@@ -1,7 +1,30 @@
 import type { Storage, StorageValue } from "vinxi/storage";
-import { prefixStorage } from "vinxi/storage";
+import { createStorage, defineDriver, prefixStorage } from "vinxi/storage";
+
+const DRIVER_NAME = "MOCK_MEMORY";
+const memory = defineDriver(() => {
+  return {
+    name: DRIVER_NAME,
+    hasItem: () => false,
+    getItem: () => null,
+    getItemRaw: () => null,
+    setItem: () => undefined,
+    setItemRaw: () => undefined,
+    removeItem: () => undefined,
+    getKeys: () => [],
+    clear: () => undefined,
+    dispose: () => undefined,
+  };
+});
 
 export function useStorage<T extends StorageValue = StorageValue>(base = ""): Storage<T> {
+  if (!globalThis.cachePlugin) {
+    // mocked storage
+    console.warn("globalThis.cachePlugin is not defined");
+    createStorage({
+      driver: memory({}),
+    });
+  }
   return (base
     ? // @ts-ignore
       prefixStorage(globalThis.cachePlugin.storage, base)
