a8ab1d5f15c6ca6f5613ab61477db833bd5cdf31
Jeysef <josimich7@gmail.com>
2025-04-24 13:30:35 +0200

    fix: add netlify blobs driver for cache storage in CachePlugin

--- src/plugins/cache.ts
+++ src/plugins/cache.ts
@@ -1,10 +1,13 @@
 import type { NitroApp } from "nitropack/types";
-import { createStorage, fsDriver, memoryDriver } from "vinxi/storage";
+import { createStorage, fsDriver, } from "vinxi/storage";
+import netlifyBlobsDriver from "unstorage/drivers/netlify-blobs";
 
-export default async function sdas(nitroApp: NitroApp) {
+export default async function CachePlugin(nitroApp: NitroApp) {
   globalThis.cachePlugin = {
     storage: createStorage({
-      driver: import.meta.env.DEV ? await fsDriver({ base: ".nitro" }) : await memoryDriver(),
+      driver: import.meta.env.DEV ? await fsDriver({ base: ".nitro" }) : await netlifyBlobsDriver({
+        name: "fitsch-cache",
+      }),
     }),
     captureError: nitroApp.captureError,
   };
