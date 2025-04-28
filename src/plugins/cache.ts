cd801bdce646a2f13ac7338f94b52d8c4209cb62
Jeysef <josimich7@gmail.com>
2025-04-28 16:16:31 +0200

    fix: refactor driver selection logic in CachePlugin for clarity

--- src/plugins/cache.ts
+++ src/plugins/cache.ts
@@ -1,13 +1,15 @@
 import type { NitroApp } from "nitropack/types";
-import { createStorage, fsDriver, } from "vinxi/storage";
+import { createStorage, fsDriver, memoryDriver } from "vinxi/storage";
 import netlifyBlobsDriver from "unstorage/drivers/netlify-blobs";
 
 export default async function CachePlugin(nitroApp: NitroApp) {
   globalThis.cachePlugin = {
     storage: createStorage({
-      driver: import.meta.env.DEV ? await fsDriver({ base: ".nitro" }) : await netlifyBlobsDriver({
-        name: "fitsch-cache",
-      }),
+      driver: import.meta.env.DEV
+        ? await fsDriver({ base: ".nitro" })  // File system driver for development
+        : process.env.NETLIFY === "true"
+          ? await netlifyBlobsDriver({ name: "fitsch-cache" }) // Netlify Blobs driver
+          : await memoryDriver(), // Memory driver for other environments
     }),
     captureError: nitroApp.captureError,
   };
