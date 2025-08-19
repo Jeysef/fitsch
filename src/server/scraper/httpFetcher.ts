a953c74f536ef2bc27ab52957521cc150b63125f
Jeysef <josimich7@gmail.com>
2025-08-19 23:30:59 +0200

    feat: better fetching msg

--- src/server/scraper/httpFetcher.ts
+++ src/server/scraper/httpFetcher.ts
@@ -4,7 +4,7 @@ export class HttpFetcher {
   constructor(private readonly fetcher: typeof fromURL) {}
 
   public async getDocument(url: string) {
-    console.debug("fetching document:", url);
+    const debugMsg = `fetching document: ${url}`;
     const controller = new AbortController();
     const timeout = setTimeout(() => controller.abort(), 6000);
 
@@ -12,14 +12,17 @@ export class HttpFetcher {
       const $ = await this.fetcher(url, {
         requestOptions: { method: "GET", signal: controller.signal, maxRedirections: 1 },
       });
-      console.debug("fetching document:", url, "OK");
+      debugMsg.concat(" OK");
       return $;
     } catch (error: any) {
+      debugMsg.concat(" ERROR");
       if (error.name === "AbortError") {
+        debugMsg.concat(" ABORTED");
         throw new Error(`Fetch from ${url} timed out.`);
       }
       throw new Error(`Failed to fetch from ${url}: ${error.message || error}`);
     } finally {
+      console.debug(debugMsg);
       clearTimeout(timeout);
     }
   }
