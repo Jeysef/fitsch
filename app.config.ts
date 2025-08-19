c8ae87b57f3ab6c5fb124f406b1384e60814b521
Jeysef <josimich7@gmail.com>
2025-08-19 23:48:23 +0200

    feat: add rollup-plugin-visualizer

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
+  vite: {
+    plugins: [visualizer({ open: true, gzipSize: true, template: "sunburst" })],
+  },
   server: {
     plugins: ["./src/plugins/cache.ts"],
   },
