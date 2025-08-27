c147ed6d90473c9ba8eb4bb44a8432ea3acf40db
Jeysef <josimich7@gmail.com>
2025-08-27 22:39:27 +0200

    feat: event checkbox bottom

--- src/components/scheduler/event/Event.tsx
+++ src/components/scheduler/event/Event.tsx
@@ -76,14 +76,14 @@ export const EventWrapper: FlowComponent<EventWrapperProps> = (props) => {
         rest.class
       )}
     >
-      <Collapsible open={!(event.collapsed || event.hidden)} class="group data-[expanded]:visible">
+      <Collapsible open={!(event.collapsed || event.hidden)} class="group data-[expanded]:visible flex flex-col h-full">
         <CardHeader class="flex-row w-full p-3 relative space-y-0">
           {event.checked && searchParams.tab !== "resultSchedule" && (
             <div class={"absolute top-0 left-0 bottom-0 w-1 bg-green-500  group-data-[expanded]:hidden"} />
           )}
           {local.header}
         </CardHeader>
-        <CollapsibleContent class={"w-full p-3 pt-0 grow flex flex-col -mt-3"} as={CardContent}>
+        <CollapsibleContent class={"w-full p-3 pt-0 grow flex flex-col -mt-3 h-full"} as={CardContent}>
           {local.children}
         </CollapsibleContent>
       </Collapsible>
