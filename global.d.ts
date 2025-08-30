b57c330ee4292bc935a555f64f514b810dd21607
Jeysef <josimich7@gmail.com>
2025-08-30 23:15:44 +0200

    fix: types in cache

--- global.d.ts
+++
@@ -1,5 +0,0 @@
-import type { CachePluginData } from "~/plugins/cache.types";
-
-declare global {
-  var cachePlugin: CachePluginData;
-}
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="@solidjs/start/env" />
+
+import type { CachePluginData } from "~/plugins/cache.types";
+
+declare global {
+  var cachePlugin: CachePluginData;
+}
