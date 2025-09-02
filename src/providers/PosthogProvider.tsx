35840e6b5eaacd62ab7993a56fae275cc99b1b55
Jeysef <josimich7@gmail.com>
2025-09-02 18:26:20 +0200

    disable posthog on dev

--- src/providers/PosthogProvider.tsx
+++ src/providers/PosthogProvider.tsx
@@ -1,11 +1,11 @@
 import posthog from "posthog-js";
 import type { FlowProps } from "solid-js";
-import { isServer } from "solid-js/web";
+import { isDev, isServer } from "solid-js/web";
 import { env } from "~/env/client";
 import { PostHogProvider as PHProvider } from "~/lib/posthog";
 
 export function PostHogProvider({ children }: FlowProps) {
-  if (isServer) return children;
+  if (isServer || isDev) return children;
   posthog.init(env.VITE_PUBLIC_POSTHOG_KEY, {
     api_host: "/api/insights", // proxy
     defaults: "2025-05-24",
