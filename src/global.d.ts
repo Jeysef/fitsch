bf14eb298abfecdc6568cf85f217f3d45d6d9e92
Jeysef <josimich7@gmail.com>
2026-04-17 11:09:44 +0200

    fix: remove cache artifacts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,8 @@
 /// <reference types="@solidjs/start/env" />
 
-import type { CachePluginData } from "~/plugins/cache.types";
 import type { BeforeInstallPromptEvent } from "~/types";
 
 declare global {
-  var cachePlugin: CachePluginData;
-
   // PWA
   interface WindowEventMap {
     beforeinstallprompt: BeforeInstallPromptEvent;
